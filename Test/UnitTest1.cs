a038e7cc352963772725efb45b2372151642385f
Dawid Hawryluk <dawid.hawryluk@soflab.pl>
2019-09-10 21:15:19 +0200

    appsettings

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,22 +1,31 @@
+
+using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.IO;
 
 namespace Tests
 {
+    [TestFixture]
     public class Tests
     {
         public IWebDriver driver;
         [SetUp]
         public void Setup()
         {
-            driver = new ChromeDriver(@"C:\Users\d_hawryluk\Desktop\");
+            driver = new ChromeDriver(@"C:\Users\d_hawryluk\Desktop");
         }
 
         [Test]
         public void Test1()
         {
-            driver.Navigate().GoToUrl("https://google.com/");
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("C:\\Users\\d_hawryluk\\source\\repos\\Test\\Test\\appsettings.json");
+            var connectionStringConfig = builder.Build();
+            driver.Navigate().GoToUrl("https://google.com");
+            driver.FindElement(By.Name("q")).SendKeys(connectionStringConfig.GetSection("Data").Value);
         }
     }
 }
